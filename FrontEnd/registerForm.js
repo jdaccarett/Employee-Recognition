f766d25b98e1fb6d4f2bfab068782eeeea50b4c2
Natalia <natyquin83@gmail.com>
2017-10-04 17:09:24 -0700

    admin/user registration - initial validation

--- FrontEnd/registerForm.js
+++ FrontEnd/registerForm.js
@@ -1,3 +1,17 @@
-$(document).ready(function(){
-alert("Hello World");
-});
+function openForm(evt, formType){
+	var tabContent, tabLink, i;
+
+	/*Hide forms */
+	tabContent = document.getElementsByClassName("tabContent");
+	for(i=0; i<tabContent.length; i++){
+		tabContent[i].style.display="none";
+	}
+
+	tabLink = document.getElementsByClassName("tabLink");
+	for (i=0; i<tabLink.length; i++){
+		tabLink[i].className=tabLink[i].className.replace("active","");
+	}
+
+	document.getElementById(formType).style.display="block";
+	evt.currentTarget.className += "active";
+}
