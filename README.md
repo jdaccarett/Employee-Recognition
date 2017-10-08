7f494082964db47d25ac16aae7852fce76c88d98
guillaug-osu <guillaug@onid.oregonstate.edu>
2017-10-08 23:35:21 +0000

    READ-ME - update readme and fix landing page

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Eridanus/Employee-Recognition
+
+Employee-Recognition is a node.js database-backed website used to track employee recognition
+awards. Employee-Recognition provides the user with a tool for managing and creating 
+employee recognition awards. The awards take the form of PDF certificates that are 
+emailed to the winners.
+
+## Getting Started
+After you clone this repo to your desktop, go to its root directory. You must have node version 
+6.0.0 and npm intalled on your machine.You can check your version of node by running `node -v`.
+
+Then install all packages with `sudo npm install`
+Run the app with `sudo npm start`
+Navigate to `http://localhost:8080`
+
+## Built With
+
+* NodeJS
+* ExpressJS
+
+## Running the tests
+
+To run the unit tests run `mocha tests`
+
+## Authors
+
+* **Juan**                  -  **jdaccarett**
+* **Natalia**               -  **naquech**
+* **Gerlin Guillaume**      -  **guillaug-osu**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- routes/index.js
+++ routes/index.js
@@ -1,78 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var expressValidator = require('express-validator');
-var bcrypt = require('bcrypt');
-const saltRounds = 10;
-const fileUpload = require('express-fileupload');
 
 /* GET home page. */
-router.get('/register', function(req, res, next) {
-  res.render('register', { title: 'Registration' });
-});
-
-/* POST request to register */
-router.post('/register', function(req, res, next) { // Here we add our user to our database.
-
-  // Using express-validator to validate our user input from our Registration form input
-  req.checkBody('name', 'Username field cannot be empty.').notEmpty();
-  req.checkBody('name', 'Username must be between 4-15 characters long.').len(3, 25);
-  req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
-  req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4, 100);
-  req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
-  req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8, 100);
-  req.checkBody('passwordMatch', 'Passwords do not match, please try again.').equals(req.body.password);
-
-  const errors = req.validationErrors();
-
-  //Check image upload if fails
-  if (!req.files)
-  return res.status(400).send('No files were uploaded.');
-  var file = req.files.signature;
-  var img_name=file.name;
-
-  // Check for Errors
-  if(errors){
-    console.log(`errors: ${JSON.stringify(errors)}`);
-    res.render('register', {
-      title: 'Registration Error',
-      errors: errors
-    });
-  }
-  // If no Errors store uploaded Image and add user info to Mysql
-  else {
-    //Variables from Registration Form
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
-    const Region = req.body.Region;
-    const img = req.files.signature;
-    const db = require('../db.js');
-
-    // Hash password entry before inserting into database
-    bcrypt.hash(password, saltRounds, function(err, hash){
-          //check the uploaded image is the right format.
-          if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
-
-            //move the upload image to upload_images folder and save the directory which will be saved by storing in the users DB.
-             file.mv('public/images/upload_images/'+file.name, function(err) {
-                  if (err) return res.status(500).send(err);
-                  //Insert Into User Table (Registration Form).
-                  db.query('INSERT INTO user (name, Region, email, password, img) VALUES (?,?,?,?,?)', [name, Region, email, hash, img_name], function(error, results, fields){
-                      //if error throw alert us
-                      if(error) throw error;
-                      //if data inserted succesfuly return us to Registrationpage.
-                      res.render('register', { title: 'Registration Complete' });
-                 })
-
-             });
-
-          }
-
-      });
-
-    }
-
+router.get('/', function(req, res, next) {
+  res.render('home');
 });
 
 module.exports = router;
