055ed8156ea60b4af454806592eb66c93b9f7b46
guillaug-osu <guillaug@onid.oregonstate.edu>
2017-10-17 03:43:04 +0000

    Remove_Yarn - remove unncessary calls to port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 //set port
-var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 8080;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -157,9 +157,9 @@ hbs.registerHelper('json', function(context) {
     return JSON.stringify(context, null, 2);
 });
 
-app.listen(port, function() {
-  console.log();
-  console.log("go to http://localhost:" + port);
-});
+// app.listen(port, function() {
+//   console.log();
+//   console.log("go to http://localhost:" + port);
+// });
 
 module.exports = app;
