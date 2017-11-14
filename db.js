2d6292231df18b0975b0b95d0e056fcf8952fb1e
jdaccarett <jdaccarett101@gmail.com>
2017-11-14 10:01:40 -0800

    Updated database

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
 
     connectionLimit : 10,
-    host            : 'us-cdbr-iron-east-03.cleardb.net',
-    user            : 'b8391048aec836',
-    password        : '1518f015',
-    database        : 'heroku_956de627ca379e5'
+    host            : '',
+    user            : '',
+    password        : '',
+    database        : ''
 
 });
 
